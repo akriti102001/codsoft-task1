b4943a702988084d2488b7879679b8f68c73a811
akriti102001 <150172922+akriti102001@users.noreply.github.com>
2023-11-28 21:56:13 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,12 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+    // Example decision: Change header color based on a condition
+    var condition = true; // Change this condition as needed
+
+    var header = document.querySelector('header');
+
+    if (condition) {
+        header.style.backgroundColor = '#'; // Green color
+    } else {
+        header.style.backgroundColor = '#3498db'; // Default blue color
+    }
+});*/
